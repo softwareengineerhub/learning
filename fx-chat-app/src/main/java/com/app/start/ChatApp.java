ba1e1fc0ef3c158b8c22fbd30a963ed8711613ff
denis4321 <denis4321@ukr.net>
2019-05-13 20:21:03 +0300

    Add UI

--- fx-chat-app/src/main/java/com/app/start/ChatApp.java
+++ fx-chat-app/src/main/java/com/app/start/ChatApp.java
@@ -1,10 +1,17 @@
 package com.app.start;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class ChatApp extends Application {
 
@@ -15,18 +22,32 @@ public class ChatApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello");
-
-
+        TextField textField = new TextField();
+        Label label = new Label("Label");
         Button button = new Button();
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //label.setText("Hello from EventHandler");
+                String text = textField.getText();
+                label.setText(text);
+            }
+        });
+
         button.setText("Button");
 
-        StackPane stackPane = new StackPane();
+
+
+
+        /*StackPane stackPane = new StackPane();
         stackPane.getChildren().add(button);
+        stackPane.getChildren().add(label);
+        Scene scene = new Scene(stackPane, 400, 600);*/
 
-        Scene scene = new Scene(stackPane, 400, 600);
-
+        VBox vBox = new VBox(10);
+        vBox.getChildren().addAll(button, label, textField);
+        Scene scene = new Scene(vBox, 400, 600);
         primaryStage.setScene(scene);
-
         primaryStage.show();
 
     }
