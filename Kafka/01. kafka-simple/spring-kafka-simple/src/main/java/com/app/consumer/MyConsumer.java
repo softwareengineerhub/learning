5036d0981a87899925144d3bd82bd18b3ab09788
softwareengineerhub <denis.prokopiuk@yahoo.com>
2022-03-28 11:12:59 +0300

    Commit

--- Kafka/01. kafka-simple/spring-kafka-simple/src/main/java/com/app/consumer/MyConsumer.java
+++ Kafka/01. kafka-simple/spring-kafka-simple/src/main/java/com/app/consumer/MyConsumer.java
@@ -1,15 +1,19 @@
 package com.app.consumer;
 
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.kafka.support.KafkaHeaders.OFFSET;
 
 @Component
 public class MyConsumer {
 
     @KafkaListener(topics = "t_topic", groupId = "default-spring-consumer")
-    public void onMessage(String message){
-        System.out.println(hashCode()+" ;message="+message);
+    public void onMessage(String message, @Header(OFFSET) long offset, KafkaConsumer kafkaConsumer){
+        System.out.println(hashCode()+" ;message="+message+"; OFFSET="+offset+"; consumer="+kafkaConsumer);
     }
 }
