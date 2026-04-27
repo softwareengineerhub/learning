1abb6dab40bf280e65199e134dfbd1b7006ddcc9
softwareengineerhub <denis.prokopiuk@yahoo.com>
2026-04-27 07:06:01 +0200

    Commit

---
+++ Kafka-21/03. kafka-json/spring-kafka-21-pause-resume/src/test/java/com/app/springkafka21pauseresume/SpringKafka21PauseResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springkafka21pauseresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafka21PauseResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
