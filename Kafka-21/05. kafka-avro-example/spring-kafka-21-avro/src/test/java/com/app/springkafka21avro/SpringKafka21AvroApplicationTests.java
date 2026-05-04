527a399e1c4b7f62b8b904386a7aef893ac8ccb5
softwareengineerhub <denis.prokopiuk@yahoo.com>
2026-05-04 19:48:24 +0200

    Commit

---
+++ Kafka-21/05. kafka-avro-example/spring-kafka-21-avro/src/test/java/com/app/springkafka21avro/SpringKafka21AvroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springkafka21avro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafka21AvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
