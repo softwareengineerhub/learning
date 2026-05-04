de03f6ad38bf9149d3683ee2094cfa08c96e0f55
softwareengineerhub <denis.prokopiuk@yahoo.com>
2026-05-04 19:43:35 +0200

    Commit

---
+++ Kafka-21/02. kafka-with-keys/spring-kafka-21-with-keys/src/main/java/com/app/springkafka21withkeys/SpringKafka21WithKeysApplication.java
@@ -0,0 +1,13 @@
+package com.app.springkafka21withkeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafka21WithKeysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafka21WithKeysApplication.class, args);
+    }
+
+}
---
+++ Kafka-21/02. kafka-with-keys/spring-kafka-21-with-keys/src/test/java/com/app/springkafka21withkeys/SpringKafka21WithKeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springkafka21withkeys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafka21WithKeysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
