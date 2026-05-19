c92799818dcf9c91580d83fbb22cb774e49008d7
softwareengineerhub <denis.prokopiuk@yahoo.com>
2026-05-19 17:18:42 +0200

    Commit

---
+++ Kafka-21/06. kafka-simple-graceful-shutdown/kafka-graceful-shutdown/src/test/java/com/example/kafkagracefulshutdown/KafkaGracefulShutdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkagracefulshutdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaGracefulShutdownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
